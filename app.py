ae20d8461d4ea190f5b4d31c4c77254ef6044e62
Richard Ebanks <richard.ebanks@mymona.uwi.edu>
2021-02-06 11:27:28 +0800

    created my first Flask app

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
+@app.route('/')
+def home():
+    return 'My home page'
+    app.run(debug=True, host="0.0.0.0", port=8080)
+
+
 
 
 @app.errorhandler(404)
